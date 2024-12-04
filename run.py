e99767cd28aa44a959380d0e88506af6344c6806
Fabio Loche <hmll86f3@students.codeinstitute.net>
2024-12-04 15:00:36 +0000

    add google sheets api configuration and welcome message

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+from datetime import datetime
+from collections import defaultdict
+import json
+import os
+from prettytable import PrettyTable
+
+# Google Sheets Setup
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+CREDS_INFO = json.loads(os.environ['creds'])  # Decodes the environment variable
+SPREADSHEET_ID = "1jNF9dM8jqkJBCoWkHhPYtRDOtXTDtGt6Omdq5cZpX8U"  # Update with your Google Sheets ID
+SHEET_NAME = "Foglio1"  # Name of the sheet
+
+print("Welcome to the Task Logger Program!")
+
